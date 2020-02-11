0b2564db1ed822372f976449edb9d6b49c883c97
Akinola <makindeakinola22@gmail.com>
2020-02-11 13:22:35 +0100

    I just created a database for the website

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
+const express = require('express');
+const path = require('path');
+const knex = require('knex');
 const io = require("socket.io")(3000);
+const app = express();
 
 const users = {};
 
+// This is the major chat server that connects to the other servers!
+
+// This is used to ensure that the website loads files mostly the css 
+app.use(express.static(path.join(__dirname, 'src')));
+// =================================================
+
+// This returns the index page!
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/html', 'index.html'));
+})
+// =================================================
+
+// Creat a database with knex!
+var database = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'your_database_user',
+        password: 'your_database_password',
+        database: 'myapp_test'
+    }
+});
+// ================================================
 io.on("connection", socket => {
     socket.on("send-chat-message", message => {
         socket.broadcast.emit('chat-message', {
@@ -19,3 +46,5 @@ io.on("connection", socket => {
         delete users[socket.id];
     })
 })
+const PORT = process.env.PORT || 5100;
+app.listen(PORT, console.log(`Server started on ${PORT}`));
